fa1d1972b90d94e543a1c4009465ece625013f2e
TAPAEB <aleksejpushkin223@gmail.com>
2022-08-24 16:30:34 +0300

    Доработал продукты

--- routes/room.route.js
+++ routes/room.route.js
@@ -3,6 +3,7 @@ const router = Router();
 const { roomController } = require('../controllers/room.controller.js');
 
 router.post('/room', roomController.postRoom);
+// router.post('/all-room', roomController.postAllRoom);
 router.get('/room-type/:type', roomController.getRoomType);
 router.post('/room-type', roomController.postRoomType);
 
