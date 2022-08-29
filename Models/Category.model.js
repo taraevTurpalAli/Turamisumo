ee0d5a2537e225bf6bf538074f044f6cddad091d
TAPAEB <aleksejpushkin223@gmail.com>
2022-08-29 14:09:36 +0300

    Исправил ошибки на бэке

--- Models/Category.model.js
+++ Models/Category.model.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
     name: String,
-    productId: [{
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: 'Product'
-    }]
 });
 
 const Category = mongoose.model('Category', categorySchema);
--- routes/product.route.js
+++ routes/product.route.js
@@ -4,6 +4,8 @@ const { productController } = require('../controllers/product.controller.js');
 
 router.post('/product', productController.postProduct);
 router.post('/category', productController.postCategory);
+router.delete('/product/:name', productController.deleteProduct);
+
 // router.get('/room-type/:type', productController.getRoomType);
 
 
